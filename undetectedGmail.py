7f5959867aeb7fca4a5185393fc7c61295fe0fc9
erick16-max <erickgege16@gmail.com>
2022-10-06 17:18:31 +0300

    gmail script password security update

--- undetectedGmail.py
+++ undetectedGmail.py
@@ -19,13 +19,13 @@ driver = uc.Chrome(options=options)
 URL = "https://accounts.google.com/v3/signin/identifier?dsh=S1896193045%3A1664795538914883&continue=https%3A%2F%2Faccounts.google.com%2F&followup=https%3A%2F%2Faccounts.google.com%2F&passive=1209600&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=AQDHYWqq_dXCFITAnYAQbkLFM0EZkLTEEXfYB5p5k8zXCKILs5OB50VVsMJIa1VirutY9hTCUYcU6A"
 driver.get(URL)
 
-driver.find_element(By.ID,"identifierId").send_keys("philipgege8@gmail.com")
+driver.find_element(By.ID,"identifierId").send_keys("enter your gmail here")
 driver.find_element(By.CSS_SELECTOR, "#identifierNext > div > button > span").click()
 
 password_selector = "#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input"
 
 WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, password_selector)))
-driver.find_element(By.CSS_SELECTOR, password_selector).send_keys("Mazeras004")
+driver.find_element(By.CSS_SELECTOR, password_selector).send_keys("enter gmail password here")
 driver.find_element(By.CSS_SELECTOR, "#passwordNext > div > button > span").click()
 
 accounts_selector = "#gbwa > div > a"
